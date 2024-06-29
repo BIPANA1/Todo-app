5a02a11d658044a891db2ecc0409f060332b51c5
Bipana <bipanashrestha435@gmail.com>
2024-06-29 20:02:30 +0545

    todo app

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,10 +1,27 @@
-import React from 'react'
 
-const EditTodoForm = () => {
+import React, {useState} from 'react'
+
+const EditTodoForm = ({editTodo, task }) => {
+  const [value, setValue] = useState(task.task)
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+
+    editTodo(value, task.id);
+
+    setValue(" ")
+
+
+  }
   return (
-    <div>
-      
-    </div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type="text" className='todo-input' 
+        value={value}  placeholder= "Update"
+         onChange={(e) => setValue(e.target.value)} />
+        <button type='submit' className='todo-btn'>Update Task</button>
+    
+    </form>
   )
 }
 
